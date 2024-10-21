ad22ce3474afecdbae5e3c17d44018fbc1406708
Roja Shrestha <stharooza3@gmail.com>
2024-10-21 11:58:44 +0545

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,71 +1,103 @@
-import logo from './logo.svg';
 import './App.css';
-
 import { useState } from 'react';
-import { LC, UC, NC,SC} from './data/PassChar';
+import { LC, UC, NC, SC } from './data/PassChar';
 
 function App() {
-  let [uppercase,setUppercase] =useState(false)
-  let [lowercase,setLowercase] =useState(false)
-  let [number,setNumber] =useState(false)
-  let [symbols,setSymbols] =useState(false)
-  let [passwordlen,setPasswordlen]=useState(10)
-  let [fPass,setPass]= useState('')
-let createPassword=()=>{
-  let finalPass=''
-  let charSet='' 
-  if(uppercase || lowercase || number || symbols){
-      if(uppercase)charSet+= UC;
-      if(lowercase) charSet += LC;
-      if(number) charSet += NC;
-      if(symbols) charSet += SC;
+  let [uppercase, setUppercase] = useState(false);
+  let [lowercase, setLowercase] = useState(false);
+  let [number, setNumber] = useState(false);
+  let [symbols, setSymbols] = useState(false);
+  let [passwordlen, setPasswordlen] = useState(10);
+  let [fPass, setPass] = useState('');
 
-      for(let i=0;i<passwordlen;i++){
-        finalPass+=charSet.charAt(Math.floor(Math.random()*charSet.length));
+  let createPassword = () => {
+    let finalPass = '';
+    let charSet = '';
+    if (uppercase || lowercase || number || symbols) {
+      if (uppercase) charSet += UC;
+      if (lowercase) charSet += LC;
+      if (number) charSet += NC;
+      if (symbols) charSet += SC;
+
+      for (let i = 0; i < passwordlen; i++) {
+        finalPass += charSet.charAt(Math.floor(Math.random() * charSet.length));
       }
       setPass(finalPass);
-     }
-   else{
-    alert("please select one checkbox!......")
-   }
-}
+    } else {
+      alert('Please select at least one option!');
+    }
+  };
+
+  let copyPass = () => {
+    if (fPass) {
+      navigator.clipboard.writeText(fPass);
+      alert('Password copied to clipboard!');
+    } else {
+      alert('Generate a password first!');
+    }
+  };
 
   return (
     <div className='passwordBox'>
       <h2>Password Generator</h2>
-      
+
       <div className='passwordBoxIn'>
-        <input type='text' value={fPass} readOnly /> <button>Copy</button>
+        <input type='text' value={fPass} readOnly />
+        <button onClick={copyPass}>Copy</button>
       </div>
 
       <div className='passLength'>
         <label>Password Length</label>
-        <input type='number' max={20} min={10} value={passwordlen} onChange={(event)=>setPasswordlen(event.target.value)}/>
+        <input
+          type='number'
+          max={20}
+          min={10}
+          value={passwordlen}
+          onChange={(event) => setPasswordlen(event.target.value)}
+        />
       </div>
 
       <div className='passLength'>
         <label>Include uppercase letters</label>
-        <input type='checkbox' checked={uppercase} onChange={()=>setUppercase(!uppercase)}/>
+        <input
+          type='checkbox'
+          checked={uppercase}
+          onChange={() => setUppercase(!uppercase)}
+        />
       </div>
 
       <div className='passLength'>
         <label>Include lowercase letters</label>
-        <input type='checkbox' checked={lowercase} onChange={()=>setLowercase(!lowercase)}/>
-      </div>
-      <div className='passLength'>
-        <label>Include numbers</label>
-        <input type='checkbox' checked={number} onChange={()=>setNumber(!number)}/>
-      </div>
-      <div className='passLength'>
-        <label>Include symbols </label>
-        <input type='checkbox' checked={symbols} onChange={()=>setSymbols(!symbols)}/>
-      </div>
-      <div>
-        <button className='btn' onClick={createPassword} >
-         Generate password
-        </button>
+        <input
+          type='checkbox'
+          checked={lowercase}
+          onChange={() => setLowercase(!lowercase)}
+        />
       </div>
 
+      <div className='passLength'>
+        <label>Include numbers</label>
+        <input
+          type='checkbox'
+          checked={number}
+          onChange={() => setNumber(!number)}
+        />
+      </div>
+
+      <div className='passLength'>
+        <label>Include symbols</label>
+        <input
+          type='checkbox'
+          checked={symbols}
+          onChange={() => setSymbols(!symbols)}
+        />
+      </div>
+
+      <div>
+        <button className='btn' onClick={createPassword}>
+          Generate password
+        </button>
+      </div>
     </div>
   );
 }
